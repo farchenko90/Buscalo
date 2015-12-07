471f5bec0a9aa28387bccaafed50e651228043c1
farchenko90 <fandresroja@gmail.com>
2015-12-07 17:46:45 -0500

    cambio id

--- Modelos/Peticiones.js
+++ Modelos/Peticiones.js
@@ -2,7 +2,7 @@ function insertarpeticion(callback,peticion){
 	db.collection("peticion").insert(
 			[{nombre: peticion.nombre,descripcion: peticion.descripcion,
 				categoria:{id: peticion.categoriaid,nombre: peticion.nombre},
-				cliente:{id: peticion.clienteid,nombres: peticion.nombres,
+				cliente:{idcliente: peticion.idcliente,nombres: peticion.nombres,
 					apellidos: peticion.apellidos,correo: peticion.correo,telefono: peticion.telefono}
 			}],{},callback);
 }
